053bb24d2eec8bbfdef15d3d91c9cb7520def8ce
ale8270 <ale8270@yandex.ru>
2016-08-29 21:28:50 +0300

    pre release

---
+++ js/header.js
@@ -0,0 +1,11 @@
+$(function(){
+    $('.button').click(function(){
+        var id = $(this).attr('href');
+
+        $('html, body').animate({
+            scrollTop: ($(id).offset().top - 80)
+        }, 500);
+        return false;
+    });
+});
+
